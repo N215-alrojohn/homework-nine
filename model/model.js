0057b6f50525dc1fa57db8f1347c2e1872fb93f3
egguterra <allisonrmj@gmail.com>
2022-04-11 20:59:03 -0400

    init commit

---
+++ model/model.js
@@ -0,0 +1,168 @@
+//variables
+var homeContent=`<h1>Welcome!</h1><div class="home-content">
+<div class="home-text">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt
+    </p>
+</div><!--home text closing-->
+<div class="home-image"></div><!--potato field image-->
+<div class="home-text">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt
+    </p>
+</div><!--home text closing-->
+</div>
+<div class="process-box-container">
+<div class="process-box">
+<div class="process-image" style="background-image: url(../images/tractor-in-field.jpg)"></div>
+<div class="process-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </div>
+</div> <!--process box closing-->
+<div class="process-box">
+<div class="process-image" style="background-image: url(../images/featured-image-potatoes.jpg)"></div>
+<div class="process-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </div>
+</div> <!--process box closing-->
+<div class="process-box">
+<div class="process-image" style="background-image: url(../images/roasted-potatoes.jpg)"></div>
+<div class="process-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad </div>
+</div> <!--process box closing-->
+</div>`;
+var typesContent=`<div class="types-container">
+<div class="types-box">
+    <div class="image-container">
+    <div class="types-image" style="background-image: url(../images/russet.png);"></div>
+    </div>
+    <div class="types-text">
+        <h1>Russet Potato</h1>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat 
+            nulla pariatur.</p>
+    </div>
+</div>
+<div class="types-box">
+    <div class="image-container">
+    <div class="types-image" style="background-image: url(../images/red.png);"></div>
+    </div>
+    <div class="types-text">
+        <h1>Red Potato</h1>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat 
+            nulla pariatur.</p>
+    </div>
+</div>
+<div class="types-box">
+    <div class="image-container">
+    <div class="types-image" style="background-image: url(../images/idaho.jpg);"></div>
+    </div>
+    <div class="types-text">
+        <h1>Idaho Potato</h1>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat 
+            nulla pariatur.</p>
+    </div>
+</div>
+</div>`;
+var recipesContent=`<div class="recipes-container">
+<div class="recipes-box">
+    <div class="recipes-text">
+        <h1>Baked Potato</h1>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat 
+            nulla pariatur.</p>
+    </div>
+    <div class="image-container">
+        <div class="recipes-image" style="background-image: url(../images/baked.png);"></div>
+        </div>
+</div>
+<div class="recipes-box">
+    <div class="recipes-text">
+        <h1>French Fries</h1>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat 
+            nulla pariatur.</p>
+    </div>
+    <div class="image-container">
+        <div class="recipes-image" style="background-image: url(../images/french.jpg);"></div>
+        </div>
+</div>
+<div class="recipes-box">
+    <div class="recipes-text">
+        <h1>Mashed Potatoes</h1>
+        <p>Lorem ipsum dolor sit amet, 
+            consectetur adipiscing elit, sed do eiusmod 
+            tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation 
+            ullamco laboris nisi ut aliquip ex ea commodo 
+            consequat. Duis aute irure dolor in reprehenderit 
+            in voluptate velit esse cillum dolore eu fugiat 
+            nulla pariatur.</p>
+    </div>
+    <div class="image-container">
+        <div class="recipes-image" style="background-image: url(../images/mashed.jpg);"></div>
+        </div>
+</div>
+</div>`;
+var faqContent=`<div class="faq-container">
+<div class="faq-box">
+<h1>Who?</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+<div class="faq-box">
+<h1>Where?</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo.
+        Duis aute irure dolor in reprehenderit in voluptate velit esse.
+</p>
+        <div class="faq-image"></div>
+</div>
+<div class="faq-box">
+<h1>Why?</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+        aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+        Excepteur sint occaecat cupidatat non proident, 
+        sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+</div>
+</div>`;
+
+//this function is being called from the app.js and has the page name
+export function modelPageName(pgName){
+
+    $("#app").html(eval(pgName));
+}
